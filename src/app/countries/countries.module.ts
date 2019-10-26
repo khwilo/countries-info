d51f4f69f22b08bb7e07cb19900769bbbe3fe65d
Khwilo Kabaka <khwilowatai@gmail.com>
2019-10-26 13:19:17 +0300

    Add icons for search and down arrow

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
 import { CountriesRoutingModule } from './countries-routing.module';
 import { CountriesComponent } from './countries.component';
 
-
 @NgModule({
   declarations: [CountriesComponent],
-  imports: [
-    CommonModule,
-    CountriesRoutingModule
-  ]
+  imports: [CommonModule, FontAwesomeModule, CountriesRoutingModule]
 })
-export class CountriesModule { }
+export class CountriesModule {
+  constructor() {
+    library.add(faChevronDown, faSearch);
+  }
+}
