89892a34118c8ccb9c4be29e1aa584da13f11026
Khwilo Kabaka <khwilowatai@gmail.com>
2019-11-01 20:26:21 +0300

    Switch between themes in the main page

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
   logoName = 'Where in the world?';
-  mode = 'Dark Mode';
+  isDark = true;
 
   constructor() {}
 
   ngOnInit() {}
+
+  changeMode() {
+    this.isDark = !this.isDark;
+
+    const bodyContainer = document.getElementById('bodyContainer');
+    bodyContainer.classList.toggle('light-mode');
+
+    const navBar = document.getElementById('navBar');
+    navBar.classList.toggle('light-mode-element');
+
+    const logoName = document.getElementById('logoName');
+    logoName.classList.toggle('light-mode-text');
+
+    const moonIcon = document.getElementById('moonIcon');
+    moonIcon.classList.toggle('light-mode-text');
+
+    const modeName = document.getElementById('modeName');
+    modeName.classList.toggle('light-mode-text');
+
+    const searchIconContainer = document.getElementById('searchIconContainer');
+    searchIconContainer.classList.toggle('light-mode-element');
+
+    const searchInput = document.getElementById('countrySearch');
+    searchInput.classList.toggle('light-mode-input');
+
+    const countryFilter = document.getElementById('countryFilter');
+    countryFilter.classList.toggle('light-mode-element');
+
+    const regionNames = document.getElementById('region-names');
+    regionNames.classList.toggle('light-mode-element');
+
+    const countriesContainer = document.querySelectorAll('.countries');
+    countriesContainer.forEach(value =>
+      value.classList.toggle('light-mode-element')
+    );
+  }
 }
