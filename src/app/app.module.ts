ad438854962448e991816973ee7a1fd728690616
Khwilo Kabaka <khwilowatai@gmail.com>
2020-02-06 15:06:24 +0300

    Add service worker to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faMoon } from '@fortawesome/free-solid-svg-icons';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
-import { CountriesModule } from './countries/countries.module';
-
 import { AppComponent } from './app.component';
+import { CountriesModule } from './countries/countries.module';
 import { NavigationComponent } from './navigation/navigation.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -18,7 +19,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
     BrowserModule,
     FontAwesomeModule,
     CountriesModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
